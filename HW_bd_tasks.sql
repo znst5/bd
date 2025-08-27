b3edaaf7b9dd3ff68e52a89f44c316846273e06a
znst5 <161199179+znst5@users.noreply.github.com>
2025-08-27 18:29:48 +0300

    task 3

---
+++ HW_bd_tasks.sql
@@ -0,0 +1,69 @@
+---Задание 2---
+
+----Название и продолжительность самого длительного трека.----
+
+select name, max(duration) max
+from treck
+group by name
+order by max desc
+limit 1;
+
+-----Название треков, продолжительность которых не менее 3,5 минут.----
+
+select name
+from treck
+where duration >= 210
+group by name, duration;
+
+----Названия сборников, вышедших в период с 2018 по 2020 год включительно.----
+
+select name, year 
+from collection
+where year between 2018 and 2020
+group by name, year;
+
+---Исполнители, чьё имя состоит из одного слова.---
+
+select name
+from artist a 
+where name not like '% %';
+
+----Название треков, которые содержат слово «мой» или «my».---
+
+select name
+from treck
+where name like '%Мой%' or name like '%My%';
+
+----Задание 3---
+
+----Количество исполнителей в каждом жанре.---
+
+select g.name, count(a.name)
+from genre g
+join genreartist gen on g.gid = gen.genreid
+join artist a on gen.artistid = a.aid
+group by g.name;
+
+----Количество треков, вошедших в альбомы 2019–2020 годов.---
+
+select count(t.name) from album a
+join treck t on t.albumid = a.albumid 
+where a.year between 2019 and 2021;
+
+---Средняя продолжительность треков по каждому альбому.--
+
+select a.name, avg(t.duration ) from album a
+join treck t on t.albumid = a.albumid
+group by a.name;
+
+---Все исполнители, которые не выпустили альбомы в 2020 году.---
+
+select ar.name from album a
+join albumartist a2 on a2.albumid = a.albumid
+join artist ar on a2.artistid = ar.aid
+where a."year" = 2020;
+
+--Названия сборников, в которых присутствует конкретный исполнитель (выберите его сами)--
+
+select * from collection c 
+where name like '%Michael Jackson%';
